7b33cb7baa9d0bd6013109ea0027f1928cad7740
gsh519 <gsh05196yu@yahoo.co.jp>
2025-02-22 16:14:16 +0900

    lesson MaxProductOfThree 修正

--- lesson6/maxProductOfThree.go
+++ lesson6/maxProductOfThree.go
@@ -6,11 +6,7 @@ func Solution(A []int) int {
 	sort.IntSlice(A).Sort()
 
 	x := A[len(A)-1] * A[len(A)-2] * A[len(A)-3]
-
-	y := 0
-	if A[0] < 0 && A[1] < 0 {
-		y = A[0] * A[1] * A[len(A)-1]
-	}
+	y := A[0] * A[1] * A[len(A)-1]
 
 	if x < y {
 		return y
